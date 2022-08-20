bc11cb7d257f8b63c2cc0fc0f30e866e5c348752
Felix <felixrunye@gmail.com>
2022-08-21 01:23:20 +0300

    title update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on AWS' });
+  res.render('index', { title: 'Felix is Awesome' });
 });
 
 module.exports = router;
